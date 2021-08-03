a963fa50369feec2a7b810673ac97fdca084d3b0
lavaspoon <lavaspoon@naver.com>
2021-08-03 11:35:26 +0900

    domain package crerate2

--- src/main/java/com/doajoa/springwebservice/domain/posts/Posts.java
+++ src/main/java/com/doajoa/springwebservice/domain/posts/Posts.java
@@ -1,4 +1,34 @@
 package com.doajoa.springwebservice.domain.posts;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
 public class Posts {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
 }
